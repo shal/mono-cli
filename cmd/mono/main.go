bc6a837515a54252f44d66e91ba9cb1ccd9411a8
Ali Shanaakh <github@shal.dev>
2022-10-11 13:21:48 +0100

    Fix

--- cmd/mono/main.go
+++ cmd/mono/main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"context"
+	"encoding/csv"
 	"errors"
 	"flag"
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/shal/mono"
+	"github.com/shopspring/decimal"
 )
 
 const DateLayout = "2006-01-02"
@@ -44,6 +47,68 @@ func New(token string) *MonoWrap {
 	}
 }
 
+func mccToENG(mcc int32) string {
+	switch mcc {
+	case 7841:
+		return "VIDEO TAPE RENTAL STORES"
+	case 4121:
+		return "TAXICABS OR LIMOUSINES"
+	case 5411:
+		return "GROCERY STORES OR SUPERMARKETS"
+	case 5812:
+		return "RESTAURANTS OR EATING PLACES"
+	case 5814:
+		return "FAST FOOD RESTAURANTS"
+	case 5912:
+		return "PHARMCIES OR DRUG STORES"
+	case 4829:
+		return "MONEY ORDERS -- WIRE TRANSFER"
+	case 7399:
+		return "BUSINESS SERVICES (NEC)"
+	case 5941:
+		return "SPORTING GOODS STORES"
+	case 5946:
+		return "CAMERA AND/OR PHOTOGRAPHIC SUPPLY STORES"
+	case 5651:
+		return "FAMILY CLOTHING STORES"
+	case 5541:
+		return "GAS/SERVICE STATIONS WITH/WITHOUT ANCILLARY SERVICES"
+	case 4812:
+		return "TELECOMMUNCATIONS EQUIPMENT INCLUDING TELEPHONE SALES"
+	case 4814:
+		return "TELECOMMUNCATIONS SERV INCL LOCA/LONG DIST CREDIT & FAX"
+	}
+
+	return "UNKNOWN"
+}
+
+func mccToCategory(mcc int32) string {
+	switch mcc {
+	case 7841:
+		return "Fun"
+	case 4121:
+		return "Transport/Taxi"
+	case 5411:
+		return "Food"
+	case 5812, 5814:
+		return "Restaurants"
+	case 5912:
+		return "Selfcare"
+	case 7399:
+		return "OpenCars"
+	case 5941, 5946, 5651:
+		return "Clothes"
+	case 5541:
+		return "Car/Gas"
+	case 4812, 4814:
+		return "Utility bills/Mobile"
+	case 4829:
+		return "Other"
+	}
+
+	return "Other"
+}
+
 func (w *MonoWrap) FindMainUAH(ctx context.Context) (*mono.Account, error) {
 	user, err := w.mono.User(ctx)
 	if err != nil {
@@ -51,7 +116,7 @@ func (w *MonoWrap) FindMainUAH(ctx context.Context) (*mono.Account, error) {
 	}
 
 	for i, acc := range user.Accounts {
-		if acc.Type == mono.Black || acc.Type == mono.Platinum {
+		if acc.Type == mono.Platinum {
 			ccy, err := mono.CurrencyFromISO4217(acc.CurrencyCode)
 			if err != nil {
 				continue
@@ -70,9 +135,11 @@ func (w *MonoWrap) FindMainUAH(ctx context.Context) (*mono.Account, error) {
 func main() {
 	to := DateFlag{date: time.Now()}
 	from := DateFlag{to.date.Add(-24 * 31 * time.Hour)}
+	var output string
 
 	flag.Var(&from, "from", "Start time")
 	flag.Var(&to, "to", "Finish time")
+	flag.StringVar(&output, "output", "result.csv", "Output format")
 
 	flag.Parse()
 
@@ -88,16 +155,54 @@ func main() {
 		log.Fatal(err)
 	}
 
-	for i := 0; i < len(txs); i++ {
-		tx := txs[len(txs)-1-i]
+	ext := filepath.Ext(output)
+	if ext != ".csv" {
+		for i := 0; i < len(txs); i++ {
+			tx := txs[len(txs)-1-i]
 
-		fmt.Printf("Number: %d\n", i+1)
-		fmt.Printf("ID: %s\n", tx.ID)
-		fmt.Printf("Amount: %0.2f UAH\n", float64(tx.Amount)/100.0)
-		fmt.Printf("Description: %s\n", tx.Description)
-		fmt.Printf("Date: %s\n", time.Unix(int64(tx.Time), 0).Format(time.RFC822))
-		fmt.Printf("MCC: %d\n", tx.MCC)
-		fmt.Printf("Amount: %0.2f UAH\n", float64(tx.Balance)/100.0)
-		fmt.Println()
+			fmt.Printf("Number: %d\n", i+1)
+			fmt.Printf("ID: %s\n", tx.ID)
+			fmt.Printf("Amount: %0.2f UAH\n", float64(tx.Amount)/100.0)
+			fmt.Printf("Description: %s\n", tx.Description)
+			fmt.Printf("Date: %s\n", tx.Time.Format(time.RFC3339))
+			fmt.Printf("MCC: %d\n", tx.MCC)
+			fmt.Printf("Amount: %0.2f UAH\n", float64(tx.Balance)/100.0)
+			fmt.Println()
+		}
+
+		return
 	}
+
+	csvFile, err := os.Create(output)
+	if err != nil {
+		log.Fatalf("failed creating file: %s", err)
+	}
+	defer csvFile.Close()
+
+	csvwriter := csv.NewWriter(csvFile)
+
+	for _, tx := range txs {
+		ccy, err := mono.CurrencyFromISO4217(acc.CurrencyCode)
+		if err != nil {
+			log.Fatalf("failed to parse currency: %s", err)
+		}
+
+		amount := decimal.NewFromInt(tx.Amount)
+		amount = amount.Shift(-2)
+
+		row := []string{
+			tx.Time.Format("02/01/2006 15:04"),
+			tx.Description,
+			amount.String(),
+			// strconv.FormatInt(int64(tx.MCC), 10),
+			ccy.Code,
+			mccToCategory(tx.MCC),
+		}
+
+		if err := csvwriter.Write(row); err != nil {
+			log.Fatalf("failed to write csv: %s", err)
+		}
+	}
+
+	csvwriter.Flush()
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/shal/mono-cli
 
 go 1.14
 
-require github.com/shal/mono v0.2.2
+require (
+	github.com/shal/mono v0.2.2
+	github.com/shopspring/decimal v1.3.1 // indirect
+)
